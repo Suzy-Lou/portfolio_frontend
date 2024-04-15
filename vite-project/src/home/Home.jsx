f91f62cf66d356a550abdc44aa9eeceb5758f7b2
Suzy-Lou <Suzy-Lou.Gervot.Etu@univ-lemans.fr>
2024-04-15 14:08:38 +0200

    mise en place du projet

---
+++ vite-project/src/home/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css'
+// import { useState } from 'react'
+
+function Home() {
+    return (
+        <>
+          <div>
+            Hello home page
+          </div>
+        </>
+      )
+}
+
+export default Home
